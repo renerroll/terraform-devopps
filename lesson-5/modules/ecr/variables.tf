97b4e67e63596eb2aa4fe7cc8d2abae2d78d4fdf
Artem <renerroll@gmail.com>
2025-09-06 23:07:05 +0300

    *hw07

--- lesson-5/modules/ecr/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "ecr_name" {
-  description = "Назва ECR репозиторію"
-  type        = string
-}
-
-variable "scan_on_push" {
-  description = "Увімкнути сканування під час пушу"
-  type        = bool
-}
